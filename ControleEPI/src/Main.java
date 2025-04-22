a5ff3148bae24632f7daac8b2ceb19701719ad93
GABRIELLA MAUREA BENTO <gabriella_bento@estudante.sesisenai.org.br>
2025-04-22 20:02:44 -0300

    projeto controle de epi com jdbc criado

---
+++ ControleEPI/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        EPIDao dao = new EPIDao();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o nome do EPI:");
+        String nome = sc.nextLine();
+        System.out.println("Digite a validade do EPI (AAAA-MM-DD):");
+        String validade = sc.nextLine();
+        EPI novo = new EPI(nome, validade);
+        dao.inserirEPI(novo);
+        System.out.println("EPIs cadastrados:");
+        for (EPI epi : dao.listarEPIs()) {
+            System.out.println("ID: " + epi.getId() + " | Nome: " + epi.getNome() + " | Validade: " +
+                    epi.getValidade());
+        }
+        sc.close();
+    }
+}
